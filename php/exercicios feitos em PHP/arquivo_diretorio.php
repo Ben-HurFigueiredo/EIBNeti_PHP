6384ef8fc7ce3610431d9313e0e9e1cde051b930
Ben-HurFigueiredo <ben-hur.figueiredo@hotmail.com>
2019-03-20 18:34:39 -0300

    apagando pastas de ex resolvidos

--- php/exercicios feitos em PHP/arquivo_diretorio.php
+++
@@ -1,5 +0,0 @@
-<h3>Envio de Arquivos</h3>
-<form method="post" action="upload.php" enctype="multipart/form-data">
-	<input type="file" name="arquivo" required>
-	<input type="submit" value="Upload">
-</form>
--- php/exercicios feitos em PHP/requisicao.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-/* requisição do arquivo comandos_saida.php */
-require_once("comandos_saida.php");
